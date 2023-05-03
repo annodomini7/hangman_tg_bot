c1c752be30710a223afacf705dffd77c21627e22
Anna <ann2004stv@yandex.ru>
2023-05-03 15:35:05 +0300

    replics in special file and found a way to send photos

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # @test_bot_py
 import logging
-from telegram import Update
+from telegram import Update, InputMediaDocument
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 from my_token import TOKEN
 from constants import *
 from find_word import find_word
+from texts import *
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -38,14 +39,14 @@ async def conversation_handler(update: Update, context: ContextTypes.DEFAULT_TYP
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
+    # await context.bot.send_document(chat_id=update.effective_chat.id, document=open("src/all_pictures.jpg", "rb"))
+    # await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open("src/all_pictures.jpg", "rb"))
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="Write \"/start_a_game\" to start a game."
-                                        " After this I will tell you a number of letters in guessed word."
-                                        " After this you should write letters. If you write a letter, which ...")
+                                   text=help_text)
 
 
 async def start_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -53,12 +54,12 @@ async def start_game(update: Update, context: ContextTypes.DEFAULT_TYPE):
         word = find_word()
         context.user_data[word_key] = word
         context.user_data[number_of_lives] = max_lives
-        context.user_data[guessed_word] = ['_ ' for i in range(len(context.user_data[word_key]))]
+        context.user_data[guessed_word] = ['_ ' for _ in range(len(context.user_data[word_key]))]
         context.user_data[all_letters] = ''
         context.user_data[game_over] = 'false'
         await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text="I thought of a word " + ''.join(
-                                           context.user_data[guessed_word]) + ". Write a letter")
+                                       text="I guessed a word " + ''.join(
+                                           context.user_data[guessed_word]) + ". Now, please, write a letter")
     except:
         await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text="Something went wrong. Please, try again later.")
@@ -71,26 +72,28 @@ async def guess_word(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # обработка некорректного ввода
     if len(context.user_data[last_written_letter]) > 1 or not context.user_data[last_written_letter].isalpha():
         await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text="you have written smth incorrect. try again")
+                                       text=incorrect_input)
         return 1
     else:
         # проверить, есть ли буква среди уже загаданных, если нет, то обработать
         context.user_data[last_written_letter] = context.user_data[last_written_letter].lower()
         if context.user_data[last_written_letter] in context.user_data[all_letters]:
             await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text="you have written this letter already. try again")
+                                           text=incorrect_input)
             return 2
         if context.user_data[last_written_letter] not in context.user_data[word_key]:
             if context.user_data[number_of_lives] == 1:
                 await context.bot.send_message(chat_id=update.effective_chat.id,
-                                               text="incorrect letter... your lives wasted. it was a word " +
+                                               text="Unfortunately, this letter is not in the guessed word..."
+                                                    " Your lives wasted. It was a word " +
                                                     context.user_data[word_key])
                 return 3
             context.user_data[number_of_lives] -= 1
             context.user_data[all_letters] += context.user_data[last_written_letter]
             await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text="incorrect letter... you have " + str(
-                                               context.user_data[number_of_lives]) + " lives now. Guessed word is " +
+                                           text="Unfortunately, this letter is not in the guessed word..."
+                                                " Now you have " + str(context.user_data[number_of_lives]) +
+                                                " lives. Guessed word is " +
                                                 ''.join(context.user_data[guessed_word]))
             return 4
         context.user_data[all_letters] += context.user_data[last_written_letter]
@@ -105,8 +108,9 @@ async def guess_word(update: Update, context: ContextTypes.DEFAULT_TYPE):
             context.user_data[game_over] = 'true'
             return 5
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="Guessed word " + ''.join(context.user_data[guessed_word]) + ". you have " +
-                                        str(context.user_data[number_of_lives]) + " lives now. write a letter")
+                                   text="Yes, there is this letter in my word. Guessed word is " +
+                                        ''.join(context.user_data[guessed_word]) + ". You have " +
+                                        str(context.user_data[number_of_lives]) + " lives now. Write next letter.")
 
 
 if __name__ == '__main__':
---
+++ texts.py
@@ -0,0 +1,8 @@
+help_text = "I'm a bot for playing Hangman. " \
+            " Write \"/start_a_game\" to start a game." \
+            " After that, I will guess a word and tell you a number of letters in guessed word." \
+            " After that, you should write the letters that you think are in this word." \
+            " If you write a letter, which is in my word, I will show you on which positions this letter stand." \
+            " Otherwise, your number of lives will decrease."
+
+incorrect_input = "You have written something incorrect. Please, try again. You should write an english letter."
